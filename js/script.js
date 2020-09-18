8fc904276279d84604a82bfa744c4d3f1cca610e
Ascalabi <evian.apollonio@gmail.com>
2020-09-18 17:55:35 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function openSLide() {
+    document.getElementById('menu').style.width = '250px';
+    document.getElementById('content').style.marginRight = '250px';
+
+}
+function closeSLide() {
+    document.getElementById('menu').style.width = '0';
+    document.getElementById('content').style.marginRight = '0';
+
+}
