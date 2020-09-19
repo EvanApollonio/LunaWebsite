066551ca0e1a14c1e7d40294e7cce5d4b548cd10
Ascalabi <evian.apollonio@gmail.com>
2020-09-19 17:03:26 +0200

    modified:   include/1.png 	new file:   include/about.png 	modified:   include/btn_next.png 	modified:   include/btn_prev.png 	new file:   include/instagram.png     new file:   include/lunaapollonio.png 	new file:   include/platnica.png 	new file:   include/x.png 	modified:   index.html 	modified:   js/script.js 	modified:   style.css

--- js/script.js
+++ js/script.js
@@ -8,3 +8,19 @@ function closeSlideMenu() {
     document.getElementById('content').style.marginRight = '0';
 
 }
+
+var open = false;
+function surprise() {
+    if (open == false) {
+        var open = true;
+        document.getElementById('foot').style.height = '100px';
+        document.getElementById('footerContent').style.marginBottom = '100px';
+        document.getElementById('surprise').style.height = '150px';
+    } else {
+        document.getElementById('foot').style.height = '0px';
+        document.getElementById('footerContent').style.marginBottom = '0px';
+        document.getElementById('surprise').style.height = '50px';
+    }
+
+    
+}
