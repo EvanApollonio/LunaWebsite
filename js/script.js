e1e32631e18ae396f3096a0209ddf8370ecaf0bd
Ascalabi <evian.apollonio@gmail.com>
2020-09-25 20:58:41 +0200

    Added close in contacts, tweaked mars

--- js/script.js
+++ js/script.js
@@ -10,12 +10,27 @@ function closeSlideMenu() {
 }
 
 function surprise() {
-    document.getElementById('foot').style.height = '10%';
-    document.getElementById('footerContent').style.marginBottom = '10%';
+    document.getElementById('foot').style.height = '12%';
+    document.getElementById('footerContent').style.marginBottom = '12%';
     document.getElementById('surprise').style.height = '15%';
+    document.getElementById('marsIm').style.bottom = '12%';
 
-    document.getElementById('marsIm').style.bottom = '9%';
 
+    document.getElementById('surpriseBackground').style.backgroundColor = '#e2e1ce';
 
+    document.getElementById('closeSurprise').style.visibility = 'visible';
+}
+
+function closeSurprise() {
     
+    document.getElementById('foot').style.height = '0%';
+    document.getElementById('footerContent').style.marginBottom = '0%';
+    document.getElementById('surprise').style.height = '0%';
+    document.getElementById('marsIm').style.bottom = '-100%';
+
+
+    document.getElementById('surpriseBackground').style.color = '#e2e1ce';
+    document.getElementById('surpriseBackground').style.opacity = '';
+
+    document.getElementById('closeSurprise').style.visibility = 'hidden';
 }
