261d31b3d8ac52152749318fc2deb8fe9a9b1126
Ascalabi <evian.apollonio@gmail.com>
2020-09-26 18:20:54 +0200

    improved sliders, added mars text, improved mars

--- js/script.js
+++ js/script.js
@@ -1,36 +1,38 @@
 function openSlideMenu() {
-    document.getElementById('menu').style.width = '25%';
+    document.getElementById('menu').style.right = '0%';
     document.getElementById('content').style.marginRight = '25%';
 
 }
 function closeSlideMenu() {
-    document.getElementById('menu').style.width = '0';
+    document.getElementById('menu').style.right = '-25%';
     document.getElementById('content').style.marginRight = '0';
 
 }
 
 function surprise() {
-    document.getElementById('foot').style.height = '12%';
+    document.getElementById('foot').style.bottom = '0%';
     document.getElementById('footerContent').style.marginBottom = '12%';
+
     document.getElementById('surprise').style.height = '15%';
-    document.getElementById('marsIm').style.bottom = '12%';
+    document.getElementById('mars').style.bottom = '10%';
 
 
-    document.getElementById('surpriseBackground').style.backgroundColor = '#e2e1ce';
+    document.getElementById('surpriseBackground').style.zIndex = '4';
+    document.getElementById('surpriseBackground').style.visibility = 'visible';
 
     document.getElementById('closeSurprise').style.visibility = 'visible';
 }
 
 function closeSurprise() {
     
-    document.getElementById('foot').style.height = '0%';
+    document.getElementById('foot').style.bottom = '-12%';
     document.getElementById('footerContent').style.marginBottom = '0%';
     document.getElementById('surprise').style.height = '0%';
-    document.getElementById('marsIm').style.bottom = '-100%';
+    document.getElementById('mars').style.bottom = '-100%';
 
 
-    document.getElementById('surpriseBackground').style.color = '#e2e1ce';
-    document.getElementById('surpriseBackground').style.opacity = '';
+    document.getElementById('surpriseBackground').style.zIndex = '-1';
+    document.getElementById('surpriseBackground').style.visibility = 'hidden';
 
     document.getElementById('closeSurprise').style.visibility = 'hidden';
 }
