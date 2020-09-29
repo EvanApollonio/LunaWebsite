011eedaf997c7c3473db9a3185676234b650aed9
Ascalabi <evian.apollonio@gmail.com>
2020-09-29 16:59:22 +0200

    improved mars, added arrow, added logo link

--- js/script.js
+++ js/script.js
@@ -13,26 +13,47 @@ function surprise() {
     document.getElementById('foot').style.bottom = '0%';
     document.getElementById('footerContent').style.marginBottom = '12%';
 
-    document.getElementById('surprise').style.height = '15%';
-    document.getElementById('mars').style.bottom = '10%';
+    document.getElementById('surprise').style.bottom = '15%';
+    document.getElementById('surprise').style.opacity = '0';
+    document.getElementById('surprise').style.zIndex = '5';
 
+    document.getElementById('surpriseClose').style.bottom = '15%';
+    document.getElementById('surpriseClose').style.opacity = '1';
+    document.getElementById('surpriseClose').style.zIndex = '6';
+
+    document.getElementById('marsIm').style.bottom = '10%';
+    document.getElementById('marsTxt').style.bottom = '65%';
 
     document.getElementById('surpriseBackground').style.zIndex = '4';
     document.getElementById('surpriseBackground').style.visibility = 'visible';
 
     document.getElementById('closeSurprise').style.visibility = 'visible';
+
+    document.getElementById('contactArrow').style.visibility = 'hidden';
 }
 
 function closeSurprise() {
     
     document.getElementById('foot').style.bottom = '-12%';
     document.getElementById('footerContent').style.marginBottom = '0%';
-    document.getElementById('surprise').style.height = '0%';
-    document.getElementById('mars').style.bottom = '-100%';
+
+    document.getElementById('surprise').style.bottom = '2%';
+    document.getElementById('surprise').style.opacity = '1';
+    document.getElementById('surprise').style.zIndex = '6'
+
+    document.getElementById('surpriseClose').style.bottom = '1.8%';
+    document.getElementById('surpriseClose').style.opacity = '0';
+    document.getElementById('surpriseClose').style.zIndex = '5'
+
+    document.getElementById('marsIm').style.bottom = '-100%';
+    document.getElementById('marsTxt').style.bottom = '-75%';
+
 
 
     document.getElementById('surpriseBackground').style.zIndex = '-1';
     document.getElementById('surpriseBackground').style.visibility = 'hidden';
 
     document.getElementById('closeSurprise').style.visibility = 'hidden';
+
+    document.getElementById('contactArrow').style.visibility = 'visible';
 }
